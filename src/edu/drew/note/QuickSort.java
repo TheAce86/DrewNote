2a133de134b7480f2cac36d502b862a5547d2c62
Joseph Pignataro <pignatarojoseph@yahoo.com>
2015-12-02 14:26:42 -0500

    skeleton quick sort class

---
+++ src/edu/drew/note/QuickSort.java
@@ -0,0 +1,16 @@
+package edu.drew.note;
+
+public class QuickSort implements SortInterface {
+
+	@Override
+	public Note[] sort(Note[] s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
